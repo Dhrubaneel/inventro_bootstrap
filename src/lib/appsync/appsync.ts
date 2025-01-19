3bbbf8c91537f51bdfbaf88021f7e63a4ddbcfa4
Dhrubaneel <dhrubaneel@gmail.com>
2025-01-19 23:55:05 +0530

    iam role issue fix

--- src/lib/appsync/appsync.ts
+++ src/lib/appsync/appsync.ts
@@ -1,11 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class AppSyncApi extends Construct {
 
     public readonly api: appsync.GraphqlApi;
     public readonly dataSource: appsync.CfnDataSource;
+    public readonly role: iam.Role;
     constructor(scope: Construct, id: string, props: AppSyncApiProps) {
         super(scope, id);
 
@@ -22,10 +24,19 @@ export class AppSyncApi extends Construct {
             }
         });
 
+        this.role = new iam.Role(this, "AppSyncRole", {
+            assumedBy: new iam.ServicePrincipal("appsync.amazonaws.com"),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSAppSyncPushToCloudWatchLogs"),
+                iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonDynamoDBFullAccess")
+            ]
+        });
+
         this.dataSource = new appsync.CfnDataSource(this, "GraphQLApiDataSource", {
             apiId: this.api.apiId,
             name: `${props?.name}_data_source`,
             type: "AMAZON_DYNAMODB",
+            serviceRoleArn: this.role.roleArn,
             dynamoDbConfig: {
                 tableName: `${props?.name}`,
                 awsRegion: cdk.Stack.of(this).region
