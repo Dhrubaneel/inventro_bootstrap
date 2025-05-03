49ff72551c527d14604f45c635f6f9800123bdfd
Dhrubaneel <dhrubaneel@gmail.com>
2025-05-03 12:53:41 +0530

    issue fix

--- backend/services/inventro_shopping_list/dbHelper.js
+++ backend/services/inventro_shopping_list/dbHelper.js
@@ -6,3 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const envPath = path.resolve(__dirname, '../../.env');
 config({ path: envPath });
+
+export async function syncCloudShoppingList(nextToken = undefined) {
+    
+}
